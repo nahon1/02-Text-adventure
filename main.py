f8f6c0365a5f5f74c2cce7dd2f32f5acad838b53
nahon1 <60366732+nahon1@users.noreply.github.com>
2020-02-05 18:33:08 -0500

    Summary
    Completes the text game

---
+++ main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import sys, os, json
+# Check to make sure we are running the correct version of Python
+assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
+
+# The game and item description files (in the same folder as this script)
+game_file = 'zork.json'
+item_file = 'items.json'
+
+
+# Load the contents of the files into the game and items dictionaries. You can largely ignore this
+# Sorry it's messy, I'm trying to account for any potential craziness with the file location
+def load_files():
+    try:
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        with open(os.path.join(__location__, game_file)) as json_file: game = json.load(json_file)
+        with open(os.path.join(__location__, item_file)) as json_file: items = json.load(json_file)
+        return (game,items)
+    except:
+        print("There was a problem reading either the game or item file.")
+        os._exit(1)
+def render(game,current):
+    c = game[current]
+    print("You are at the " + c["name"])
+    print(c["desc"])
+
+def get_input():
+    response = input("What do you want to do? ")
+    response = response.upper().strip()
+    return response
+
+def update(game,current,response):
+    c = game[current]
+    for e in c["exits"]:
+        if response == e["exit"]:
+            return e["target"]
+    return current
+# The main function for the game
+def main():
+    current = 'Start'  # The starting location
+    end_game = ['Corporation']  # Any of the end-game locations
+
+    (game,items) = load_files()
+
+    # Add your code here
+while True:
+        render(game,current)
+
+        for e in end_game:
+            if current == e:
+                print("Here comes 667! If you don't understand then you chose the wrong path! ")
+                break #break out of the while loop
+
+        response = get_input()
+
+        if response == "QUIT" or response == "Q":
+            break #break out of the while loop
+
+        current = update(game,current,response)
+
+    print("Have a nice day!")
+# run the main function
+if __name__ == '__main__':
+	main()
